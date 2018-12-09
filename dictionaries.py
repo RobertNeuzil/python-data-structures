de5a3738181251450716c2d6e5f10e679d3e6cf2
robert neuzil <robertneuzil@gmail.com>
2018-12-09 00:00:11 -0500

    first

---
+++ dictionaries.py
@@ -0,0 +1,20 @@
+d = {}
+
+# dictionary keys treat integers and strings differently
+d[1] = 'yes'
+d['1'] = 'no'
+d[2] = 90000000000
+
+class MyClass:
+	def __init__(self):
+		self.data = 'data'
+
+#create an instance of class
+
+instance = MyClass()
+
+#can store the instance in a dictionary
+d['object'] = instance
+
+d['object'].data
+print ( d.keys() )
