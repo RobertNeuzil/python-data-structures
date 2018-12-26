2890c1a72c3e8731493d9313ded9e5921414c55c
robert neuzil <robertneuzil@gmail.com>
2018-12-26 03:36:27 -0500

    balanced parenthesis

--- stackdatastruct.py
+++ stackdatastruct.py
@@ -24,14 +24,3 @@ class Stack():
 	def end(self):
 		if not self.is_empty():
 			return self.items[-1]
-
-s = Stack()
-s.push("A")
-s.push("B")
-s.push("C")
-print (s.get_stack())
-s.pop()
-s.push("D")
-print (s.get_stack())
-print(s.peek())
-print (s.end())
